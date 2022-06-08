839233ac6c0462d17331af0de36b44a2faf91f42
alm933 <leonmartinezalejandro8@gmail.com>
2022-06-08 11:25:56 -0700

    this is just an advance in the login

---
+++ app/src/main/java/com/upn/proyectaw/VistadePrueba2.java
@@ -0,0 +1,14 @@
+package com.upn.proyectaw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VistadePrueba2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vistade_prueba2);
+    }
+}
