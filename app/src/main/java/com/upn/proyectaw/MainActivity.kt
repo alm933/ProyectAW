048461401b10b6c91464287d4a6c7dac44a64d6d
alm933 <leonmartinezalejandro8@gmail.com>
2022-06-08 11:25:45 -0700

    this is just an advance in the login

--- app/src/main/java/com/upn/proyectaw/MainActivity.kt
+++ app/src/main/java/com/upn/proyectaw/MainActivity.kt
@@ -5,8 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ActionMode
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -18,5 +25,26 @@ class MainActivity : AppCompatActivity() {
             val lanzar = Intent(this, RegistrarActivity::class.java)
             startActivity(lanzar)
         }
+
+        val btnlogin = findViewById<Button>(R.id.buttonLogin);
+        btnlogin.setOnClickListener {
+            var usr = findViewById<EditText>(R.id.textEmail);
+            val pss = findViewById<EditText>(R.id.passw);
+
+            if(usr.getText().toString().equals("user")&&pss.getText().toString().equals("pass")){
+
+                val lanzar = Intent(this, VistadePrueba2::class.java)
+                startActivity(lanzar)
+
+
+            }else{
+                Toast.makeText(this,"cREDENCIALES INCRORRECTAS",Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+
+
+
     }
 }
