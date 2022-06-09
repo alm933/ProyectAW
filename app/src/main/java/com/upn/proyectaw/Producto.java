788f4a35ee5e72a585eaba6f5d82cbe314203b8b
alm933 <leonmartinezalejandro8@gmail.com>
2022-06-09 14:32:42 -0700

    this is just an advance in the login v.2.1 cambio de imagenes - idea de  como ImageView a imagen deseada. se Puede cambiar la imagen a el producto deseado

--- app/src/main/java/com/upn/proyectaw/Producto.java
+++ app/src/main/java/com/upn/proyectaw/Producto.java
@@ -4,19 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class Producto extends AppCompatActivity {
+public class Producto extends AppCompatActivity{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_producto);
 
+        int analgesico = getIntent().getIntExtra("analgesico",1);
 
-        ImageView i = findViewById(R.id.imageProducto);
-        i.setImageResource(R.drawable.gcopia);
-
-
+        if (analgesico == 1) {
+            ImageView i = findViewById(R.id.imageProducto);
+            i.setImageResource(R.drawable.analgesico512);
+        }else{
+            Toast.makeText(this,"no se pudo",Toast.LENGTH_LONG).show();
+        }
 
     }
 }
--- app/src/main/java/com/upn/proyectaw/VistadePrueba2.java
+++ app/src/main/java/com/upn/proyectaw/VistadePrueba2.java
@@ -8,8 +8,8 @@ import android.widget.Button;
 import android.view.View;
 
 public class VistadePrueba2 extends AppCompatActivity {
-
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vistade_prueba2);
@@ -18,8 +18,13 @@ public class VistadePrueba2 extends AppCompatActivity {
         producto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+
+             int analgesico = 1;
+
                 Intent i = new Intent(VistadePrueba2.this , Producto.class);
+                i.putExtra("analgesico" ,1);
                 startActivity(i);
+
             }
         });
     }
